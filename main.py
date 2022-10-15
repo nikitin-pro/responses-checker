e67c57ca6891291f4e9dab653214def56d16dda7
nikitin-pro <95876501+nikitin-pro@users.noreply.github.com>
2022-10-15 20:39:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import io
+import streamlit
+from PIL import Image
+import imagehash
+
+def get_hash(img):
+    hash = imagehash.average_hash(img)
+    return hash
+
+
+def load_image(lbl):
+    uploaded_file = streamlit.file_uploader(label=lbl, key=lbl)
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        streamlit.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+
+streamlit.title('Get image hash')
+img0 = load_image('Upload correct response')
+img1 = load_image('Upload image to test')
+result = streamlit.button('Test')
+if result:
+    hash0 = get_hash(img0)
+    hash1 = get_hash(img1)
+    delta = hash0 - hash1
+    streamlit.write('**Difference:**')
+    streamlit.write(delta)
