90de55d494e42fad3d06be85bab6169c7b95ff59
nikitin-pro <95876501+nikitin-pro@users.noreply.github.com>
2022-10-16 17:59:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit
 from PIL import Image
 import imagehash
 
+correct_hash
+
 def get_hash(img):
     hash = imagehash.average_hash(img)
     return hash
@@ -13,31 +15,27 @@ def load_image_correct():
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         streamlit.image(image_data)
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
+        img = Image.open(io.BytesIO(image_data))
+        correct_hash = get_hash(img)
+        
 
 def load_image():
-    uploaded_file = streamlit.file_uploader(label='Upload images to test', key=1, type=['png','jpg'], accept_multiple_files=True)
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalues()
-        streamlit.image(image_data[0])
-        return Image.open(io.BytesIO(image_data[0]))
+    uploaded_files = streamlit.file_uploader(label='Upload images to test', key=1, type=['png','jpg'], accept_multiple_files=True)
+    if uploaded_files is not None:
+        with streamlit.container():
+            for file in uploaded_files:
+                image_data = file.getvalue()
+                streamlit.image(image_data)
+                img = Image.open(io.BytesIO(image_data))
+                hash = get_hash(img)
+                delta = correct_hash - hash
+                resu = 'PASSED!' if delta <= 1 else 'FAILED!'
+                streamlit.write(resu)
     else:
         return None
 
 
 streamlit.title('Get image hash')
-img0 = load_image_correct()
-img1 = load_image()
-result = streamlit.button('Check')
-if result:
-    hash0 = get_hash(img0)
-    hash1 = get_hash(img1)
-    delta = hash0 - hash1
-    if delta <= 1:
-        resu = 'PASSED!'
-    else:
-        resu = 'FAILED!'
-    streamlit.write('**Verdict:**')
-    streamlit.write(resu)
+load_image_correct()
+load_image()
+
